2bc9016d0e1967424798a0458789fab140bea390
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-04-10 15:37:48 -0500

    Video

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -198,8 +198,8 @@
       
       figure.mb-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video: Leyenda del video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/_fBRYZs4E8I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        
 
     ol.lista-ol--cuadro.mb-4
       li
